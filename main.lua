a6bbe8a4349fc2bfda78d71c1146713e61977458
TwoPandazOneCup <lsvest@gmail.com>
2016-10-24 12:35:20 +0200

    splitta spelar

--- main.lua
+++ main.lua
@@ -2,30 +2,7 @@ function love.load(arg)
   BULLETWIDTH = 10
   gameState = "playing"
 
-  spelar = {
-    x = 100,
-    y = 0,
-    z = 100,
-    width = 60,
-    height = 60,
-    fart = 500,
-    hoppHogd = 200,
-    cooldown = 20,
-    bulletSpeed = 5,
-    bullets =  {},
-
-    fire = function()
-      bullet = {
-        x = spelar.x + spelar.width /2 - BULLETWIDTH / 2,
-        y = spelar.y - spelar.height
-        }
-
-      if spelar.cooldown == 0 then
-        table.insert(spelar.bullets, bullet)
-        spelar.cooldown = 20
-      end
-    end
-  }
+  dofile "spelar.lua"
 
 end
 
--- spelar.lua
+++ spelar.lua
@@ -1,1 +1,24 @@
-shfsaj
+spelar = {
+  x = 100,
+  y = 0,
+  z = 100,
+  width = 60,
+  height = 60,
+  fart = 500,
+  hoppHogd = 200,
+  cooldown = 20,
+  bulletSpeed = 5,
+  bullets =  {},
+
+  fire = function()
+    bullet = {
+      x = spelar.x + spelar.width /2 - BULLETWIDTH / 2,
+      y = spelar.y - spelar.height
+      }
+
+    if spelar.cooldown == 0 then
+      table.insert(spelar.bullets, bullet)
+      spelar.cooldown = 20
+    end
+  end
+}
